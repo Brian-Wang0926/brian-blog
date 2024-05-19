20309f7e807314d7107d8f80649ad6b019d96c0e
Brian-Wang0926 <aa0988402763@gmail.com>
2024-05-19 17:04:28 +0800

    Stage 3: Site Header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "BrianBlog",
+  url: "https://brianblog.vercel.app",
+  description: "Brian's Blog",
+  author: "Brian",
+  links: {
+    twitter: "",
+    github: "",
+    personalSite: "",
+  },
+};
+
+export type siteConfig = typeof siteConfig
