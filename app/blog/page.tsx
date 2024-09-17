1ac52b3d7032f8035db46eb750ab3488a0340e33
Brian-Wang0926 <aa0988402763@gmail.com>
2024-09-17 17:20:11 +0800

    Stage 10: Pagination

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,13 +1,24 @@
 import { posts } from "#site/content";
 import { PostItem } from "@/components/post-item";
+import { QueryPagination } from "@/components/query-pagination";
 import { sortPosts } from "@/lib/utils";
 
-export default async function BlogPage() {
-  const sortedPosts = sortPosts(posts.filter((post) => post.published));
-  const displayPosts = sortedPosts;
+const POSTS_PER_PAGE = 5;
 
-  // 添加打印輸出檢查
-  console.log("displayPosts", displayPosts);
+interface BlogPageProps {
+  searchParams: {
+    page?: string;
+  };
+}
+export default async function BlogPage({ searchParams }: BlogPageProps) {
+  const currentPage = Number(searchParams.page) || 1;
+  const sortedPosts = sortPosts(posts.filter((post) => post.published));
+  const totalPages = Math.ceil(sortedPosts.length / POSTS_PER_PAGE);
+
+  const displayPosts = sortedPosts.slice(
+    POSTS_PER_PAGE * (currentPage - 1),
+    POSTS_PER_PAGE * currentPage
+  );
 
   return (
     <div className="container max-w-4xl py-6 lg:py-10">
@@ -41,6 +52,10 @@ export default async function BlogPage() {
           ) : (
             <p>No posts yet</p>
           )}
+          <QueryPagination
+            totalPages={totalPages}
+            className="justify-end mt-4"
+          />{" "}
         </div>
       </div>
     </div>
