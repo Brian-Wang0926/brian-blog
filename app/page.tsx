7b79ee56b7647e19b38fd9ffdbe735c797cb9bd3
WangBoYuan <boyuan.wang@sinopac.com>
2025-03-27 16:11:39 +0800

    刪除首頁最近文章區

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import { buttonVariants } from "@/components/ui/button";
 import { siteConfig } from "@/config/site";
-import { cn, sortPosts } from "@/lib/utils";
-import { posts } from "#site/content";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { PostItem } from "@/components/post-item";
 
 export default function Home() {
-  const latestPosts = sortPosts(posts).slice(0, 5);
   return (
     <>
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
@@ -39,24 +36,6 @@ export default function Home() {
           </Link>
         </div>
       </section>
-      <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6 mt-60 ">
-        <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
-          Latest Posts
-        </h2>
-      </section>
-      <ul className="flex flex-col px-10">
-        {latestPosts.map((post) => (
-          <li key={post.slug} className="first:border-t first:border-border">
-            <PostItem
-              slug={post.slug}
-              title={post.title}
-              description={post.description}
-              date={post.date}
-              tags={post.tags}
-            />
-          </li>
-        ))}
-      </ul>
     </>
   );
 }
